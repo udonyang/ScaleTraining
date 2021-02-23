bbbad2bc565f2c9009b9440f36b858f572196d6b
Dwylkz <xellosgeteternal@gmail.com>
2021-02-23 18:52:13 +0800

    up

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import time
 import random
 
 def main():
+    MARK = ['+', '*']
+
     cps = int(sys.argv[1])
     rnd = int(sys.argv[2])
+    print("\n")
     for i in range(0, rnd):
         scale = ['C', 'bD', 'D', 'bE', 'E', 'F', 'bG', 'G', 'bA', 'A', 'bB', 'B']
         random.shuffle(scale)
         for note in scale:
-            print(i, ": ", note, '\r', end='')
+            for x in range(0, 15):
+                print(MARK[rnd%2], end='')
+            print(" ", rnd, ": ", note, '\r', end='')
             time.sleep(60/cps)
 
 main()
